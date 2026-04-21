9007c87c9611cba22ea90b7e03576634fff443c4
Brandon Moore <16313090+thatcube@users.noreply.github.com>
2026-04-20 20:47:00 -0400

    Debug: add click raycast logging to diagnose interaction issue

--- src/modules/purifier.js
+++ src/modules/purifier.js
@@ -2006,6 +2006,7 @@ export function createPurifier(scene) {
     _mouse.y=-((clientY-rect.top)/rect.height)*2+1;
     _raycaster.setFromCamera(_mouse,camera);
     const hits=_raycaster.intersectObjects(scene.children,true);
+    console.log('[purifier] handleClick, hits:', hits.length, hits.slice(0,3).map(h => h.object.constructor.name + (h.object._isLamp ? ' LAMP' : '') + (h.object._isCeilLight ? ' CEIL' : '') + (h.object._isWindow ? ' WIN' : '')));
     // Find the first interactive hit (skip objects inside invisible parent groups)
     let obj=null;
     for(const h of hits){
